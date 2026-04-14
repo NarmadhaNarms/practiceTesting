2fba13abdc0a18d569813fdbb1edcc811e7226d1
narmadha <narmadha.557@gmail.com>
2026-04-14 18:56:14 +0530

    sum

---
+++ hello.py
@@ -0,0 +1,7 @@
+print ("hello narms")
+name = input("enter your neme:")
+print ("welcome beautiful", name)
+
+a = int(input("enter a number: "))
+b = int(input("enter another number: "))
+print("sum:", a + b)
---
+++ sum.py
@@ -0,0 +1,5 @@
+a = int(input("enter a number: "))
+b = int(input("enter another number: "))
+print("sum:", a + b)
+print("minus:", a-b)
+print("multiply:", a*b)
