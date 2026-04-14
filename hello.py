93c0aa6859679ec9ce0537266892823d623b233a
narmadha <narmadha.557@gmail.com>
2026-04-14 19:18:35 +0530

    beautifull removed

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 print ("hello narms")
 name = input("enter your neme:")
-print ("welcome beautiful", name)
+print ("welcome", name)
 
 a = int(input("enter a number: "))
 b = int(input("enter another number: "))
