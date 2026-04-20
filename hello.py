26becb66d08841ea5e6f5d0e0b4ffdbffd202609
narmadha <narmadha.557@gmail.com>
2026-04-20 12:37:16 +0530

    sum c added

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 print ("hello narms")
 name = input("enter your neme:")
-print ("welcome", name)
+print ("welcome AI", name)
 
 a = int(input("enter a number: "))
 b = int(input("enter another number: "))
