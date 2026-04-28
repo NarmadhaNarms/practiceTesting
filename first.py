94281a6bfe903651f3f619b0962b51139d09109f
narmadha <narmadha.557@gmail.com>
2026-04-28 13:34:04 +0530

    name age added

---
+++ first.py
@@ -0,0 +1,8 @@
+name = input("enter your name:")
+##print("welcome",name)
+
+#name =input("enter your name:")
+#age=input("enter your age:")
+#print("your name is",name,"and your age is",age)
+if 5>2:
+ print("five is greater than 2")
